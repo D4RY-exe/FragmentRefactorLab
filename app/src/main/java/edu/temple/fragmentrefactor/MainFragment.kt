6eadae1b05f34299ce40fd32cee680db5f689473
D4RY-exe <129776733+D4RY-exe@users.noreply.github.com>
2025-03-12 16:00:47 -0400

    Created fragment layout and trying to code Main

--- app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainFragment : Fragment() {
 
@@ -16,4 +19,20 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+    }
 }
